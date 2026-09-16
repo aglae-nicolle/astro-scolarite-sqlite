730ff2af39198bd6da918b7a10f57a33d21fae53
AglaeNcl <aglaencl@gmail.com>
2026-09-16 14:14:13 +0200

    Initial Commit

---
+++ src/lib/db.ts
@@ -0,0 +1,15 @@
+import { mkdirSync } from "node:fs";
+import { dirname, resolve } from "node:path";
+
+import { Database } from "bun:sqlite";
+const path = resolve(
+  import.meta.env.SQLITE_DB_PATH || "./data/scolarite.sqlite"
+);
+console.log({ path });
+
+mkdirSync(dirname(path), { recursive: true });
+const db = new Database(path);
+
+db.exec("PRAGMA foreign_keys = ON;");
+db.exec("PRAGMA journal_mode = WAL;");
+export default db;
